d28f37763ec1522a9e25f9e7fad5c2f9851e4944
GonzaloCorte <gcarte@ext.bancoripley.comm>
2021-05-26 23:48:38 -0300

    Estructura inicial proyecto

---
+++ src/main/java/com/gonzalo/api/PruebasTecnicasApplication.java
@@ -0,0 +1,14 @@
+package com.gonzalo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasTecnicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasTecnicasApplication.class, args);
+	
+	}
+
+}
---
+++ src/test/java/com/gonzalo/api/PruebasTecnicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzalo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasTecnicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
